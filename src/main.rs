b2a3bf45219abcc5baedc5b46100fd06bfc82204
optifat <pavsmel@mail.ru>
2021-02-05 15:21:23 +0300

    first version of bacckend

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use actix_files::Files;
+use actix_web::{HttpServer, App, web, HttpResponse, Responder};
+use tera::{Tera, Context};
+use serde::{Serialize, Deserialize};
+
+async fn index(tera: web::Data<Tera>) -> impl Responder {
+    let data = Context::new();
+    let rendered = tera.render("index.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+async fn my_projects(tera: web::Data<Tera>) -> impl Responder {
+    let data = Context::new();
+    let rendered = tera.render("my_projects.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+#[derive(Debug, Deserialize)]
+struct LoginUser {
+    username: String,
+    password: String,
+}
+
+async fn login(tera: web::Data<Tera>) -> impl Responder {
+    let data = Context::new();
+    let rendered = tera.render("login.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+async fn process_login(data: web::Form<LoginUser>) -> impl Responder {
+    println!("{:?}", data);
+    HttpResponse::Ok().body(format!("Logged in: {}", data.username))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let tera = Tera::new("templates/**/*").unwrap();
+        App::new()
+            .data(tera)
+            .route("/", web::get().to(index))
+            .route("/my_projects", web::get().to(my_projects))
+            .route("/log_in", web::get().to(login))
+            .route("/log_in", web::post().to(process_login))
+            .service(Files::new("/", "templates/").index_file("index.html"))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
